a06bfb65e366f1fbc1322c8aa7cb388fc3c46784
itiaditi <aditi.tlgt244@gmail.com>
2024-05-15 22:55:28 +0530

    remove carousel

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { Container, Row, Col, Carousel } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
+import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
-import Button from "react-bootstrap/Button";
+import ProjectCard from "./ProjectCards";
+import Github from "../About/Github";
 
 import reliance from "../../Assets/Projects/reliance.jpeg";
 import toppers from "../../Assets/Projects/toppers.jpeg";
 import sb from "../../Assets/Projects/snapbasket.jpeg";
-import Github from "../About/Github";
 
 function Projects(props) {
   return (
@@ -20,55 +19,47 @@ function Projects(props) {
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
-        <Carousel>
-          <Carousel.Item>
-            <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-              <Col md={12} className="project-card">
-                <ProjectCard
-                  imgPath={sb}
-                  isBlog={false}
-                  title="Zepto Clone"
-                  description="SnapBasket, is your one-stop online destination for all your grocery needs."
-                  link="https://debug-adept-0123.vercel.app/snapBasket/index.html"
-                  github="https://github.com/itiaditi/debug-adept-0123"
-                  tech="HTML, CSS, JAVASCRIPT"
-                />
-              </Col>
-            </Row>
-          </Carousel.Item>
-          <Carousel.Item>
-            <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-              <Col md={12} className="project-card">
-                <ProjectCard
-                  imgPath={reliance}
-                  isBlog={false}
-                  title="Reliance Digital Clone"
-                  description="Reliance World focuses on offering a wide range of electronic products and gadgets."
-                  link="https://65ade3de9fb39fdcb10c705e--stirring-starlight-16f55e.netlify.app/"
-                  github="https://github.com/itiaditi/javascript-jadoo-8901.git"
-                  tech="HTML, CSS, BOOTSTRAP"
-                />
-              </Col>
-            </Row>
-          </Carousel.Item>
-          <Carousel.Item>
-            <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-              <Col md={12} className="project-card">
-                <ProjectCard
-                  imgPath={toppers}
-                  isBlog={false}
-                  title="Khan Academy Clone"
-                  description="Toppers Academy is a free website where you can learn about many different things."
-                  github="https://github.com/itiaditi/toppers-academy.git"
-                  link="https://toppers-academy-henna.vercel.app/"
-                  tech="ReactJs, Typescript, JSON server"
-                />
-              </Col>
-            </Row>
- </Carousel.Item>
-        </Carousel>
+        <Row style={{ justifyContent: "center" }}>
+          <Col md={12} className="project-card">
+            <ProjectCard
+              imgPath={sb}
+              isBlog={false}
+              title="Zepto Clone"
+              description="SnapBasket, is your one-stop online destination for all your grocery needs."
+              link="https://debug-adept-0123.vercel.app/snapBasket/index.html"
+              github="https://github.com/itiaditi/debug-adept-0123"
+              tech="HTML, CSS, JAVASCRIPT"
+            />
+          </Col>
+        </Row>
+        <Row style={{ justifyContent: "center" }}>
+          <Col md={12} className="project-card">
+            <ProjectCard
+              imgPath={reliance}
+              isBlog={false}
+              title="Reliance Digital Clone"
+              description="Reliance World focuses on offering a wide range of electronic products and gadgets."
+              link="https://65ade3de9fb39fdcb10c705e--stirring-starlight-16f55e.netlify.app/"
+              github="https://github.com/itiaditi/javascript-jadoo-8901.git"
+              tech="HTML, CSS, BOOTSTRAP"
+            />
+          </Col>
+        </Row>
+        <Row style={{ justifyContent: "center" }}>
+          <Col md={12} className="project-card">
+            <ProjectCard
+              imgPath={toppers}
+              isBlog={false}
+              title="Khan Academy Clone"
+              description="Toppers Academy is a free website where you can learn about many different things."
+              github="https://github.com/itiaditi/toppers-academy.git"
+              link="https://toppers-academy-henna.vercel.app/"
+              tech="ReactJs, Typescript, JSON server"
+            />
+          </Col>
+        </Row>
       </Container>
-      <Github/>
+      <Github />
     </Container>
   );
 }
